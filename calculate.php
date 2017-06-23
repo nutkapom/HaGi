9ca9025662144bce9edd825d2e1e5cfcad4f327f
nutkapom <nutkapom@gmail.com>
2017-06-23 11:36:56 +0700

    Add files via upload

---
+++ calculate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ */
+class Calculate {
+  public function add($first, $last){
+    return $first + $last;
+  }
+}
+
+
+?>
---
+++ test_calculate.php
@@ -0,0 +1,33 @@
+<?php
+include "calculate.php";
+
+class Test_calculate extends PHPUnit_Framework_TestCase{
+
+    private $calculate;
+
+    public function setup(){
+      $this->calculate = new Calculate();
+    }
+
+    /**
+    *@dataProvider additionProvider
+    */
+    public function testAdd($a, $b, $expected){
+      $this->assertEquals($expected, $this->calculate->add($a, $b));
+    }
+
+    public function additionProvider()
+    {
+      return[
+        '0+0=0' => [0,0,0],
+        '0+1=1' => [0,1,1],
+        '1+0=0' => [1,0,0],
+        '1+4=3' => [1,4,3]
+      ];
+
+    }
+
+}
+
+
+ ?>
